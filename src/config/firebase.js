876e01580f46a7bcc2390f16e9e6d0cb68ba7dad
muhamadnaufan7 <muhamadnaufan7@gmail.com>
2021-06-27 10:50:04 +0700

    semua

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+  apiKey: "AIzaSyDsUWvd_Y3NmHCRDiUde0UTpVtAiZATnmE",
+  authDomain: "belanjakuy-ea0f3.firebaseapp.com",
+  databaseURL: "https://belanjakuy-ea0f3-default-rtdb.firebaseio.com",
+  projectId: "belanjakuy-ea0f3",
+  storageBucket: "belanjakuy-ea0f3.appspot.com",
+  messagingSenderId: "136414932358",
+  appId: "1:136414932358:web:e0428633b717826c821007",
+  measurementId: "G-81MMVDTNVV"
+};
+
+export default firebaseConfig;
