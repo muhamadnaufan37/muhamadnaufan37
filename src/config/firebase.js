5ad92fdb052596f4eb882765b090cf0d428cf678
muhamadnaufan7 <muhamadnaufan7@gmail.com>
2021-06-29 09:16:37 +0700

    gakjadi

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-  apiKey: "AIzaSyC-LlcDQQA6CM2QASNEFcvD9kjDufvWTac",
-  authDomain: "belanjakuy-backup.firebaseapp.com",
-  projectId: "belanjakuy-backup",
-  storageBucket: "belanjakuy-backup.appspot.com",
-  messagingSenderId: "89989567371",
-  appId: "1:89989567371:web:1bbcb5b61c95dcfe96558e",
-  measurementId: "G-55MZKW5KE9"
+  apiKey: "AIzaSyDsUWvd_Y3NmHCRDiUde0UTpVtAiZATnmE",
+  authDomain: "belanjakuy-ea0f3.firebaseapp.com",
+  databaseURL: "https://belanjakuy-ea0f3-default-rtdb.firebaseio.com",
+  projectId: "belanjakuy-ea0f3",
+  storageBucket: "belanjakuy-ea0f3.appspot.com",
+  messagingSenderId: "136414932358",
+  appId: "1:136414932358:web:e0428633b717826c821007",
+  measurementId: "G-81MMVDTNVV"
 };
 
 export default firebaseConfig;
